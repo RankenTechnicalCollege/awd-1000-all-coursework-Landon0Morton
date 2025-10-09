86b12fcbfd013c7815df6e115e1ac61c1a8aba86
Landon0Morton <landon_morton@insideranken.org>
2025-10-09 11:54:46 -0500

    I finished the Chapter 10 Apply.

---
+++ HTML_CSS/Exercises/HTML_CH10_EX/Apply/scripts/script.js
@@ -0,0 +1,9 @@
+/* Landon Morton, Scripts.js, 10/9/25 */
+
+/* Function to display content */
+function content() {
+  var text = document.getElementById("new");
+  text.textContent = "Hi. I am a new paragraph that was created with a JavaScript function.";
+  text.style.color = "#co145f";
+  text.style.fontSize = "2em";
+}
