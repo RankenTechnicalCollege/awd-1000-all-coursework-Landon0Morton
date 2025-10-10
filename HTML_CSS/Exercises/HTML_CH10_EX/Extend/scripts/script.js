9c043b1967594e7df34b1ad8ea165a42fd341bd8
Landon0Morton <landon_morton@insideranken.org>
2025-10-10 11:02:51 -0500

    I finished Chapter 10 Extend.

---
+++ HTML_CSS/Exercises/HTML_CH10_EX/Extend/scripts/script.js
@@ -0,0 +1,13 @@
+/*
+    Student Name: Landon Morton
+    File Name: script.js
+    Date: 10/10/25
+*/
+
+function hide() {
+    document.getElementById("main").style.display = 'none';
+}
+
+function show() {
+    document.getElementById("main").style.display = 'block';
+}
