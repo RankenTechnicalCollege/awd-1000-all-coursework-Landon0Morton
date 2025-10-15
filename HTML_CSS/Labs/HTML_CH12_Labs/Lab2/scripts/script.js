be7e6a866b5ad89aae8a9c3d2e5d339613992858
Landon0Morton <landon_morton@insideranken.org>
2025-10-15 09:29:10 -0500

    I finished Chapter 12 Lab 2.

---
+++ HTML_CSS/Labs/HTML_CH12_Labs/Lab2/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Landon Morton
+    File Name: script.js
+    Date: 10/15/25
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+$('.hero').height($(window).height());
+});
