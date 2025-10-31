816f158016b8843788364bf7017faa769b708c53
Landon0Morton <landon_morton@insideranken.org>
2025-10-31 04:55:36 -0500

    I finished JavaScript Chapter 2 HOP 1

---
+++ JavaScript/data-files/Chapter02/js02/project01/project02-01.js
@@ -0,0 +1,18 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-01
+
+      Celsius <-> Farenheit Coverter
+      Author: Landon Morton
+      Date: 10/31/25
+
+      Filename: project02-01.js
+ */
+
+function FarenheitToCelsius(degree) {
+      return (degree-32)/1.8;
+};
+
+function CelsiusToFarenheit(degree) {
+      return degree*1.8+32
+};
